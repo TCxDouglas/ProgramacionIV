bce7c8bce007e0680a8c94c7df7c038b985d3a22
TCxDouglas <douglasalexander683@gmail.com>
2020-05-03 23:41:27 -0600

    Arreglando Busg y frases

--- private/Config/config.php
+++ private/Config/config.php
@@ -3,10 +3,6 @@
 *IMPORTAMOS LA CLASE PRINCIPAL DE CONEXION 
 */
 include('../../Conexion/BD.php');
-<<<<<<< HEAD
 $conexion=new BD('localhost','root','','db_sistema_peliculas');
-=======
-$conexion=new BD('localhost','root','','db_sistema_peliculas ');
->>>>>>> b4afad85d14a86b446a8d15df8db0ad0cb244cc5
 
 ?>
